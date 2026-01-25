e468e62662f322e49250a4c1efc24c6303691ce8
DHL <david.hernandez@uclm.es>
2026-01-25 11:07:06 +0100

    .

--- lib/PostGISServerAPI.py
+++ lib/PostGISServerAPI.py
@@ -328,6 +328,47 @@ class PostGISServerAPI():
             return str_error
         return str_error
 
+    def delete_layer(self, project_id, layer_id):
+        str_error = ''
+        str_error = ''
+        if self.url is None:
+            str_error = 'url is none. Connect before'
+            return str_error
+        if self.token is None:
+            str_error = 'token is none. Connect before'
+            return str_error
+        if not isinstance(project_id, int):
+            str_error = 'project_id must be an integer'
+            return str_error
+        if not isinstance(layer_id, int):
+            str_error = 'layer_id must be an integer'
+            return str_error
+        url_post = self.url + defs_server_api.URL_LAYERS + '/' + str(project_id) + '/' +str(layer_id)
+        payload = {}
+        headers_as_dict = {}
+        # headers_as_dict[defs_server_api.HEADERS_TAG_CONTENT] = defs_server_api.HEADERS_CONTENT_DEFAULT_VALUE
+        headers_as_dict[defs_server_api.HEADERS_TAG_AUTHORIZATION] = (defs_server_api.HEADERS_TAG_AUTHORIZATION_BEARER
+                                                                      + self.token)
+        # headers_as_dict[defs_server_api.HEADERS_TAG_ACCEPT] = defs_server_api.HEADERS_ACCEPT_DEFAULT_VALUE
+        # headers = json.dumps(headers_as_dict)
+        headers = headers_as_dict
+        response = requests.request("DELETE", url_post, headers=headers, data=payload)#, data=payload)
+        if response.status_code == 400:
+            str_error = 'post request failed: not found'
+            return str_error
+        response_text_as_dict = json.loads(response.text)
+        if not response.ok:
+            if not defs_server_api.RESPONSE_TEXT_TAG_MESSAGE in response_text_as_dict:
+                str_error = 'Not exists {} tag in response'.format(defs_server_api.RESPONSE_TEXT_TAG_MESSAGE)
+                return str_error
+            str_error = 'post request failed: {}'.format(response_text_as_dict[defs_server_api.RESPONSE_TEXT_TAG_MESSAGE])
+            return str_error
+        if not defs_server_api.RESPONSE_TEXT_TAG_DATA in response_text_as_dict:
+            str_error = 'Not exists {} tag in response'.format(defs_server_api.RESPONSE_TEXT_TAG_DATA)
+            return str_error
+        str_error = self.get_layers(project_id)
+        return str_error
+
     def download_file(self, project_id, target_server_file_path, target_file_path):
         str_error = ''
         if self.url is None:
